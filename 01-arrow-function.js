0edaa5d9948e699e6647c8dd380f5e756619a3db
mariana.vns <mariana.vns@gmail.com>
2021-02-05 16:25:29 -0200

    refactor(arch01): organiza as primeiras explicações sobre arrow functions.

--- 01-arrow-function.js
+++ 01-arrow-function.js
@@ -1,25 +1,36 @@
-// FUNÇÃO TRADICIONAL
+/// FUNÇÃO TRADICIONAL
 
-function somaNumeros (a, b) {
-    return a + b
-}
+        function somaNumeros (a, b) {
+            return a + b
+        }
+        console.log(somaNumeros(2,3))
 
-console.log(somaNumeros(2,3))
 
 
-// ARROW FUNCTION
+/// ARROW FUNCTION SEM CHAVES E SEM RETURN
 
-const somaNumero = (a, b) => {
-    a + b
-}
+        var somaNumero = (a, b) =>  a + b
+        console.log(somaNumero(10, 20))
+            // Não precisa de chaves nem return pq é apenas para mostrar o resultado no console. Se adicionar as chaves, só funciona com return.
 
-console.log(somaNumeros(10, 20))
+            
 
-// ARROW FUNCTION COM APENAS UM ARGUMENTO
+/// ARROW FUNCTION COM CHAVES E COM RETURN
 
-const escreveNome = nome => console.log(nome)
-escreveNome('Mariana')
+        var somaNumero33 = (a, b) => {
+            return a + b 
+        }
+        console.log(`${somaNumero33(10, 20)} é o resultado.`)
+            // Se o valor da função for usado em outro lugar, a arrow function PRECISA ter chaves e um return. PRECISA.
 
-const somaCom10 = numero => numero + 10
-console.log(somaCom10(40))
+
+
+/// ARROW FUNCTION COM APENAS UM ARGUMENTO
+
+        const escreveNome = nome => console.log(nome)
+        escreveNome('Mariana')
+
+        const somaCom10 = numero => numero + 10
+        console.log(somaCom10(40))
+            // Os parenteses no argumento são dispensáveis.
 
